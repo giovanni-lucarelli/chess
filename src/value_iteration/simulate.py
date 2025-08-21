0a11db6ce5dc229756650bba6545779126c2f767
CapraCampa <destasiovaleria16@gmail.com>
2025-08-22 00:14:11 +0200

    added alternative way to test policy

--- src/value_iteration/simulate.py
+++ src/value_iteration/simulate.py
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt # type: ignore
 from src.value_iteration.value_iteration import ValueIteration
 from build.chess_py import Env, Move
 from chessrl.env import Env, SyzygyDefender
+from chessrl.utils.mate_positions import generate_endgames_offline
 
 import pickle
 
@@ -34,12 +35,20 @@ if __name__ == '__main__':
         
     TB_PATH = "tablebase"  
     defender = SyzygyDefender(TB_PATH) 
+
+    if config['all_checkmates']:    
+        # KR vs K, White to move, exact mate-in-2
+        fens = generate_endgames_offline("KRvK", mate_in=8, side_to_move="w",
+                                        max_positions=3, max_tries=50000)
+    else:
+        fens = config['test_endgames']
     
     logger.info('Start simulating...')
-    for fen in config['test_endgames']:
+    for fen in fens:
         env = Env.from_fen(fen, gamma = config['gamma'], step_penalty = config['step_penalty'], defender = defender)
-        
-        env.display_state()
+        logger.info("\n")
+
+        #env.display_state()
         #plt.show()
         counter = 1
         while True:
@@ -50,6 +59,7 @@ if __name__ == '__main__':
             # Check if this state exists in the policy
             if current_fen not in policy:
                 logger.error(f'Current FEN not found in policy!')
+                break
                 
             move_uci = policy[current_fen] 
             if move_uci is None:
